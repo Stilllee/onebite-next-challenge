8e44f8d86ad1ad9fa3a887872ba0cc7d2b650d83
Stilllee <orodae@gmail.com>
2024-09-13 23:26:34 +0900

    [Day 05] UI 구현 #4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
+import MovieItem from "@/components/MovieItem";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/SearchableLayout";
+import movies from "@/mock/movies.json";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col gap-12">
+      <section>
+        <h2 className="my-5 text-xl font-bold">지금 가장 추천하는 영화</h2>
+        <ul className="grid grid-cols-3 gap-2">
+          {movies.slice(0, 3).map((movie) => (
+            <li key={movie.id}>
+              <MovieItem {...movie} />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2 className="my-5 text-xl font-bold">등록된 모든 영화</h2>
+        <ul className="grid grid-cols-5 gap-x-2 gap-y-4">
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <MovieItem {...movie} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,25 @@
+import MovieItem from "@/components/MovieItem";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/SearchableLayout";
+import movies from "@/mock/movies.json";
 import { useRouter } from "next/router";
 
 export default function Page() {
   const router = useRouter();
   const { q } = router.query;
 
-  return <div>검색결과 : {q}</div>;
+  return (
+    <>
+      <h2 className="sr-only">영화 검색 결과</h2>
+      <ul className="grid grid-cols-3 gap-x-2 gap-y-4">
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <MovieItem {...movie} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
