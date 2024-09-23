45bdce0574c5341980088bb47d1512680ffa665c
Stilllee <orodae@gmail.com>
2024-09-23 17:35:10 +0900

    [Day 11] UI 구현 #10

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,29 @@
+import MovieItem from "../components/MovieItem";
+import movies from "@/mock/movies.json";
+
 export default function Home() {
-  return <div>홈 페이지</div>;
+  return (
+    <div className="flex flex-col gap-12">
+      <section>
+        <h2 className="my-5 text-xl font-bold">지금 가장 추천하는 영화</h2>
+        <ul className="grid grid-cols-3 gap-2">
+          {movies.slice(0, 3).map((movie) => (
+            <li key={`recomovie-${movie.id}`}>
+              <MovieItem {...movie} />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2 className="my-5 text-xl font-bold">등록된 모든 영화</h2>
+        <ul className="grid grid-cols-5 gap-x-2 gap-y-4">
+          {movies.map((movie) => (
+            <li key={`allmovie-${movie.id}`}>
+              <MovieItem {...movie} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,7 +1,21 @@
+import MovieItem from "@/app/components/MovieItem";
+import movies from "@/mock/movies.json";
+
 export default function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
-  return <div>검색 결과 : {searchParams.q}</div>;
+  return (
+    <>
+      <h2 className="sr-only">영화 검색 결과</h2>
+      <ul className="grid grid-cols-3 gap-x-2 gap-y-4">
+        {movies.map((movie) => (
+          <li key={`searchmovie-${movie.id}`}>
+            <MovieItem {...movie} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
