77b788e0d63d3f10e77069ee73533b54db2f6f5a
Stilllee <orodae@gmail.com>
2024-10-02 03:01:05 +0900

    [Day 12] 인덱스 페이지 데이터 페칭 #11

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,28 +1,55 @@
+import { MovieData } from "@/types";
 import MovieItem from "../components/MovieItem";
 import movies from "@/mock/movies.json";
+
+async function AllMovies() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`);
+  if (!res.ok) {
+    return <p>오류가 발생했습니다.</p>;
+  }
+  const allMovies: MovieData[] = await res.json();
+
+  return (
+    <ul className="grid grid-cols-5 gap-x-2 gap-y-4">
+      {allMovies.map((movie) => (
+        <li key={`allmovie-${movie.id}`}>
+          <MovieItem {...movie} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+async function RecoMovies() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+  );
+  if (!res.ok) {
+    return <p>오류가 발생했습니다.</p>;
+  }
+  const recoMovies: MovieData[] = await res.json();
+
+  return (
+    <ul className="grid grid-cols-3 gap-2">
+      {recoMovies.slice(0, 3).map((movie) => (
+        <li key={`recomovie-${movie.id}`}>
+          <MovieItem {...movie} />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-12">
       <section>
         <h2 className="my-5 text-xl font-bold">지금 가장 추천하는 영화</h2>
-        <ul className="grid grid-cols-3 gap-2">
-          {movies.slice(0, 3).map((movie) => (
-            <li key={`recomovie-${movie.id}`}>
-              <MovieItem {...movie} />
-            </li>
-          ))}
-        </ul>
+        <RecoMovies />
       </section>
       <section>
         <h2 className="my-5 text-xl font-bold">등록된 모든 영화</h2>
-        <ul className="grid grid-cols-5 gap-x-2 gap-y-4">
-          {movies.map((movie) => (
-            <li key={`allmovie-${movie.id}`}>
-              <MovieItem {...movie} />
-            </li>
-          ))}
-        </ul>
+        <AllMovies />
       </section>
     </div>
   );
