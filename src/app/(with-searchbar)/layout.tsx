c90b7b6a680d35f7d0b64658b47f89bab6f73ceb
Stilllee <orodae@gmail.com>
2024-10-03 17:51:29 +0900

    [Day 13] SearchBar 빌드 에러 해결을 위한 Suspense 적용 #12
    - useSearchParams 사용으로 인한 빌드 에러 해결

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
+
 import SearchBar from "../components/SearchBar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <SearchBar />
+      <Suspense fallback={<p>로딩중...</p>}>
+        <SearchBar />
+      </Suspense>
       {children}
     </>
   );
