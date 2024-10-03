5c6bc91fd54a11fcd0583020b5adb4c8c737c8c0
Stilllee <orodae@gmail.com>
2024-10-03 16:38:12 +0900

    [Day 12] 서치 페이지 데이터 패칭 #11

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,25 @@
+import { MovieData } from "@/types";
 import MovieItem from "@/app/components/MovieItem";
-import movies from "@/mock/movies.json";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${searchParams.q}`,
+  );
+  if (!res.ok) {
+    return <p>오류가 발생했습니다.</p>;
+  }
+
+  const searchMovies: MovieData[] = await res.json();
+
   return (
     <>
       <h2 className="sr-only">영화 검색 결과</h2>
       <ul className="grid grid-cols-3 gap-x-2 gap-y-4">
-        {movies.map((movie) => (
+        {searchMovies.map((movie) => (
           <li key={`searchmovie-${movie.id}`}>
             <MovieItem {...movie} />
           </li>
