fbf6364141eb139d7fdaa018ca6a9a2a878bdd47
Stilllee <orodae@gmail.com>
2024-10-04 23:51:07 +0900

    [Day 15] 서치 페이지 스트리밍 방식 변경 #14
    - Suspense를 사용하여 쿼리가 변경될 때마다 로딩 적용되도록 수정

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,10 @@
 import { MovieData } from "@/types";
 import MovieItem from "@/app/components/MovieItem";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { q?: string };
-}) {
+async function SearchResult({ q }: { q: string }) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
     {
       cache: "force-cache",
     },
@@ -19,15 +16,27 @@ export default async function Page({
   const searchMovies: MovieData[] = await res.json();
 
   return (
+    <ul className="grid grid-cols-3 gap-x-2 gap-y-4">
+      {searchMovies.map((movie) => (
+        <li key={`searchmovie-${movie.id}`}>
+          <MovieItem {...movie} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
+  return (
     <>
       <h2 className="sr-only">영화 검색 결과</h2>
-      <ul className="grid grid-cols-3 gap-x-2 gap-y-4">
-        {searchMovies.map((movie) => (
-          <li key={`searchmovie-${movie.id}`}>
-            <MovieItem {...movie} />
-          </li>
-        ))}
-      </ul>
+      <Suspense key={searchParams.q || ""} fallback={<p>로딩중...</p>}>
+        <SearchResult q={searchParams.q || ""} />
+      </Suspense>
     </>
   );
 }
