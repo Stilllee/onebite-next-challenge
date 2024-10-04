4de7396ba79091ca47855704e541b84d965c9ff8
Stilllee <orodae@gmail.com>
2024-10-05 01:40:01 +0900

    [Day 15] 서치 페이지 스켈레톤 UI 적용 #14

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import { MovieData } from "@/types";
 import MovieItem from "@/app/components/MovieItem";
+import MovieListSkeleton from "@/app/components/skeleton/MovieListSkeleton";
 import { Suspense } from "react";
 
 async function SearchResult({ q }: { q: string }) {
@@ -34,7 +35,18 @@ export default function Page({
   return (
     <>
       <h2 className="sr-only">영화 검색 결과</h2>
-      <Suspense key={searchParams.q || ""} fallback={<p>로딩중...</p>}>
+      <Suspense
+        key={searchParams.q || ""}
+        fallback={
+          <ul
+            className="grid grid-cols-3 gap-2"
+            role="status"
+            aria-live="polite"
+          >
+            <MovieListSkeleton count={3} />
+          </ul>
+        }
+      >
         <SearchResult q={searchParams.q || ""} />
       </Suspense>
     </>
