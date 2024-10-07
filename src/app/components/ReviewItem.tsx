32c31d0f3f446d8cc0ad6a8ed0c313e3ff09eb24
Stilllee <orodae@gmail.com>
2024-10-07 21:24:27 +0900

    [Day 16] 리뷰 조회 기능 구현 #15

--- src/app/components/ReviewItem.tsx
+++ src/app/components/ReviewItem.tsx
@@ -1,17 +1,22 @@
-export default function ReviewItem() {
+import { ReviewData } from "@/types";
+
+const formatDate = (dateString: string): string => {
+  return dateString.split("T")[0].replace(/-/g, ".");
+};
+
+export default function ReviewItem({ author, createdAt, content }: ReviewData) {
   return (
     <li className="mb-8 flex flex-col gap-4 border-b-2 border-obDarkLine pb-8">
       <div className="mb-3 flex items-end gap-3">
         <span className="text-lg font-bold" aria-label="리뷰 작성자">
-          작성자
+          {author}
         </span>
         <div className="text-obGray">
-          <time dateTime="2024-10-6" aria-label="작성일: 2024년 10월 6일">
-            2024.10.6
-          </time>
+          <span className="sr-only">작성일: </span>
+          <time dateTime={createdAt}>{formatDate(createdAt)}</time>
         </div>
       </div>
-      <p>리뷰</p>
+      <p>{content}</p>
       <button
         className="w-fit border-obGray text-obGray hover:border-b"
         aria-label="이 리뷰 삭제하기"
--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { MovieData } from "@/types";
+import { MovieData, ReviewData } from "@/types";
+
 import { ReviewEditor } from "@/app/components/ReviewEditor";
 import ReviewItem from "@/app/components/ReviewItem";
 
@@ -50,10 +51,9 @@ async function MovieDetail({ movieId }: { movieId: string }) {
       <h3 className="sr-only">영화 상세 정보</h3>
       <div className="space-y-2 opacity-50">
         <p>
-          <time aria-label="개봉일" dateTime={releaseDate}>
-            {releaseDate}
-          </time>{" "}
-          / <span aria-label="장르">{genres.join(", ")}</span> /{" "}
+          <span className="sr-only">개봉일: </span>
+          <time dateTime={releaseDate}>{releaseDate}</time> /{" "}
+          <span aria-label="장르">{genres.join(", ")}</span> /{" "}
           <span aria-label="상영 시간">{runtime}분</span>
         </p>
         <p aria-label="제작사">{company}</p>
@@ -66,13 +66,23 @@ async function MovieDetail({ movieId }: { movieId: string }) {
   );
 }
 
-async function ReviewList() {
+async function ReviewList({ movieId }: { movieId: string }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/movie/${movieId}`,
+  );
+
+  if (!res.ok) {
+    throw new Error(`리뷰를 가져오지 못했습니다.: ${res.statusText}`);
+  }
+
+  const reviews: ReviewData[] = await res.json();
+
   return (
     <ul>
       <h3 className="sr-only">리뷰 목록</h3>
-      <ReviewItem />
-      <ReviewItem />
-      <ReviewItem />
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
     </ul>
   );
 }
@@ -82,7 +92,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     <div className="flex flex-col gap-14">
       <MovieDetail movieId={params.id} />
       <ReviewEditor movieId={params.id} />
-      <ReviewList />
+      <ReviewList movieId={params.id} />
     </div>
   );
 }
